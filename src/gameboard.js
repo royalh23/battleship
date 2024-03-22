d18afdccfcb2c406bf34f93a191f9ad431f109d5
Royal <royalhuseyn902@gmail.com>
2024-03-22 14:31:33 +0400

    Revert to using Gameboard.createBoard Because Array.prototype.fill() creates array items referencing the same object in memory, which is not what we want

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,6 +1,20 @@
 export default class Gameboard {
   constructor() {
-    this.gb = Array(10).fill(Array(10).fill(0));
+    this.gb = Gameboard.createBoard();
+  }
+
+  static createBoard() {
+    const gb = [];
+
+    for (let row = 0; row < 10; row += 1) {
+      const rowArr = [];
+      for (let col = 0; col < 10; col += 1) {
+        rowArr.push(0);
+      }
+      gb.push(rowArr);
+    }
+
+    return gb;
   }
 
   placeShip() {}
