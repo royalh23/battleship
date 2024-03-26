be7ac3790a7882e689676fde79a1a3e5c5d8b51c
Royal <royalhuseyn902@gmail.com>
2024-03-26 15:42:54 +0400

    Refactor isSunk test to make it use it.each

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,21 +1,17 @@
 import Ship from '../ship';
 
 describe('isSunk', () => {
-  it('Returns false if ship was hit once (ship length is 4)', () => {
-    const ship = new Ship(4);
-    ship.hit();
-    expect(ship.isSunk()).toEqual(false);
-  });
-
-  it('Returns false if ship was hit twice (ship length is 4)', () => {
-    const ship = new Ship(4);
-    for (let i = 0; i < 2; i += 1) ship.hit();
-    expect(ship.isSunk()).toEqual(false);
-  });
-
-  it('Returns true if ship was hit 4 times (ship length is 4)', () => {
-    const ship = new Ship(4);
-    for (let i = 0; i < 4; i += 1) ship.hit();
-    expect(ship.isSunk()).toEqual(true);
-  });
+  it.each([
+    [false, 1],
+    [false, 2],
+    [false, 3],
+    [true, 4],
+  ])(
+    'Returns %p if ship was hit %p times (ship length is 4)',
+    (condition, hits) => {
+      const ship = new Ship(4);
+      for (let i = 0; i < hits; i += 1) ship.hit();
+      expect(ship.isSunk()).toEqual(condition);
+    },
+  );
 });
