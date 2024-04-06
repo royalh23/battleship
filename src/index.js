7bcca458ada89e41e442355ce318c1dd0bbff2f2
Royal <royalhuseyn902@gmail.com>
2024-04-06 17:51:00 +0400

    Remove class system and use functions instead

--- src/index.js
+++ src/index.js
@@ -1,57 +1,58 @@
 import './style.css';
 import Game from './game';
 
-class Display {
-  #game;
+const game = new Game();
+const boards = document.querySelector('.boards');
 
-  #boards;
+function displayBoard(board) {
+  const cont = document.createElement('div');
+  const grid = document.createElement('div');
+  const text = document.createElement('div');
 
-  constructor() {
-    this.#game = new Game();
-    this.#boards = document.querySelector('.boards');
-    this.#displayBoard(this.#game.playerBoard.board);
-    this.#addBtns();
-  }
+  cont.classList.add('cont');
+  grid.classList.add('grid');
+  text.classList.add('text');
 
-  #displayBoard(board) {
-    const boardCont = document.createElement('div');
-    boardCont.classList.add('board');
+  text.textContent =
+    board === game.playerBoard.board ? 'Your board' : "Computer's board";
 
-    board.forEach((row) => {
-      row.forEach((col) => {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        if (board === this.#game.playerBoard.board && col.data !== null)
-          cell.classList.add('ship');
-        if (col.isHit) cell.classList.add('hit');
-        boardCont.appendChild(cell);
-      });
+  board.forEach((row) => {
+    row.forEach((col) => {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      if (board === game.playerBoard.board && col.data !== null)
+        cell.classList.add('ship');
+      if (col.isHit) cell.classList.add('hit');
+      grid.appendChild(cell);
     });
+  });
 
-    this.#boards.appendChild(boardCont);
-  }
-
-  #addBtns() {
-    const btns = document.createElement('div');
-    const randomBtn = document.createElement('button');
-    const startBtn = document.createElement('button');
-
-    btns.classList.add('btns');
-    randomBtn.classList.add('btn');
-    startBtn.classList.add('btn');
-
-    randomBtn.textContent = 'Randomize';
-    startBtn.textContent = 'Start game';
-
-    btns.append(randomBtn, startBtn);
-    document.body.append(btns);
-
-    randomBtn.addEventListener('click', () => {
-      this.#boards.textContent = '';
-      this.#game.playerBoard.placeRandomly();
-      this.#displayBoard(this.#game.playerBoard.board);
-    });
-  }
+  cont.append(grid, text);
+  boards.appendChild(cont);
 }
 
-const display = new Display();
+function addBtns() {
+  const btns = document.createElement('div');
+  const randomBtn = document.createElement('button');
+  const startBtn = document.createElement('button');
+
+  btns.classList.add('btns');
+  randomBtn.classList.add('btn');
+  startBtn.classList.add('btn');
+
+  randomBtn.textContent = 'Randomize';
+  startBtn.textContent = 'Start game';
+
+  btns.append(randomBtn, startBtn);
+  document.body.append(btns);
+
+  randomBtn.addEventListener('click', () => {
+    boards.textContent = '';
+    game.playerBoard.placeRandomly();
+    displayBoard(game.playerBoard.board);
+  });
+}
+
+// Initialize display
+displayBoard(game.playerBoard.board);
+addBtns();
